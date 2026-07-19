d8d4b428ec65641560da043904ba18a5c19e35a3
Danda Keerthi <dandakeerthi2@gmail.com>
2026-07-19 10:48:06 +0530

    JavaScript

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,4 +1,12 @@
 console.log("Hello World");
 console.log("This is keerthi.I am currently a 3rd year student pursuing CSE in SIST");
 
+//template literals
+
+let a=10;
+let b=6;
+let output="sum is : "+ (a+b);
+console.log(output);
+let op=`sum is : ${a+b}`;
+console.log(op);
 

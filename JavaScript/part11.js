9f582c50c28d2585a0f276e4c41950e02d12c662
Danda Keerthi <dandakeerthi2@gmail.com>
2026-08-01 21:01:50 +0530

    part11

--- JavaScript/part11.js
+++ JavaScript/part11.js
@@ -17,3 +17,34 @@ changeColor("red", 1000, () => {
         });
     });
 });
+
+//promises
+
+function saveToDb(data,success,failure){
+    let internetSpeed=Math.floor(Math.random()*10)+1;
+    console.log(internetSpeed);
+    if(internetSpeed >=4 ){
+        success();
+    }else{
+        failure();
+    }
+    
+}
+saveToDb("Hello, this is keerthi.",()=>{
+    console.log("Success:The data was saved"); 
+
+    saveToDb("I am a 3rd yr student",()=>{
+        console.log("Success2 :The data2 was saved"); 
+
+        saveToDb("I am learning Full-Stack Web Development",()=>{
+            console.log("Success3 :The data3 was saved");
+            
+        },()=>{
+             console.log("Failure: Weak Internet ! The data was not saved");
+        })
+    },()=>{
+        console.log("Failure2: Weak Internet ! The data2 was not saved");
+    })
+},()=>{
+    console.log("Failure: Weak Internet ! The data was not saved");
+});

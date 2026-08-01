61cb4def5faf1262a8074cbc577016ec67cf0084
Danda Keerthi <dandakeerthi2@gmail.com>
2026-08-01 20:38:36 +0530

    part11

---
+++ JavaScript/part11.js
@@ -0,0 +1,19 @@
+let h1=document.querySelector("h1");
+
+//callback hell
+function changeColor(color, delay, nextColorChange) {
+    setTimeout(() => {
+        h1.style.color = color;
+        if (nextColorChange) nextColorChange();
+    }, delay);
+}
+
+changeColor("red", 1000, () => {
+    changeColor("orange", 1000, () => {
+        changeColor("green", 1000, () => {
+            changeColor("yellow", 1000, () => {
+                changeColor("blue", 1000);
+            });
+        });
+    });
+});

782db8eb1d121b54294c848a3aee9cf63fe4470e
Danda Keerthi <dandakeerthi2@gmail.com>
2026-08-02 21:27:21 +0530

    part13

--- JavaScript/part13.js
+++ JavaScript/part13.js
@@ -1,18 +1,71 @@
-let url="https://catfact.ninja/fact";
+// let url="https://catfact.ninja/fact";
 
-async function getFacts(){
+// async function getFacts(){
+//     try{
+//         let res=await axios.get(url);
+//         return res.data.fact;
+//     }catch(e){
+//         return "NO fact found!"
+//     }
+// }
+// getFacts();
+
+// let btn=document.querySelector("button");
+
+// btn.addEventListener("click",async ()=>{
+//     let fact= await getFacts();
+//     console.log(fact);
+//     let p=document.querySelector("#result");
+//     p.innerText=fact;
+// })
+
+
+//Activity using query strings
+let url2="https://universities.hipolabs.com/search?country=India";
+// let country="India";
+
+
+async function getColleges(state){
     try{
-        let res=await axios.get(url);
-        return res.data.fact;
+        // let res=await axios.get(url2+country);
+        let res=await axios.get(url2);
+        // return res.data;
+        let colleges = res.data.filter((clg) => {
+    return (
+        clg["state-province"] &&
+        clg["state-province"].toLowerCase() === state.toLowerCase()
+    );
+});
+    return colleges; 
     }catch(e){
-        return "NO fact found!"
+        console.log(e);
+        return [];
     }
 }
-getFacts();
+
 
 let btn=document.querySelector("button");
 
-btn.addEventListener("click",async ()=>{
-    let fact= await getFacts();
-    console.log(fact);
+btn.addEventListener("click",async()=>{
+    // let country=document.querySelector("input").value;
+    // console.log(country);
+
+    let state=document.querySelector("input").value;
+    console.log(state);
+
+    let clgs= await getColleges(state);
+    show(clgs);
 })
+
+function show(clgs){
+    let list=document.querySelector("#output"); //ul
+    list.innerText="";
+
+    for(clg of clgs){
+        console.log(clg.name);
+
+        let li=document.createElement("li");
+        li.innerText=clg.name;
+        list.appendChild(li);
+    }
+}

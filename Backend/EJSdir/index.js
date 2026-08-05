a77d43767c27a7fec07eb5e0b9670eda496a49e7
Danda Keerthi <dandakeerthi2@gmail.com>
2026-08-05 19:55:00 +0530

    Backend

--- Backend/EJSdir/index.js
+++ Backend/EJSdir/index.js
@@ -4,6 +4,9 @@ const app=express();
 const path=require("path");
 
 const port=3000;
+
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
 
 app.set("views",path.join(__dirname,"/views"));
@@ -19,10 +22,22 @@ app.get("/rolldice",(req,res)=>{
 })
 
 app.get("/ig/:username",(req,res)=>{
-    const followers=["hema","anu","sai","ram","rahul","abi"];
-    let {username}=req.params;
+    // const followers=["hema","anu","sai","ram","rahul","abi"];
+    // let {username}=req.params;
     // console.log(username);
-    res.render("insta.ejs",{username,followers});
+    // res.render("insta.ejs",{username,followers});
+
+    const instaData=require("./data.json");
+    let {username}=req.params;
+    // console.log(instaData);
+
+    const data=instaData[username];
+    console.log(data);
+    if(data){
+        res.render("insta.ejs",{data});
+    }else{
+        res.render("error.ejs");
+    }
 })
 
 app.listen(port,()=>{

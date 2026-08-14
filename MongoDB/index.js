b5ef2bf46abddb9b78b0cb3d880d81c41a6b9d2c
Danda Keerthi <dandakeerthi2@gmail.com>
2026-08-14 21:22:14 +0530

    MongoDB

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -19,10 +19,47 @@ const User=mongoose.model("User",userSchema);
 
 //insert one
 const user1=new User({name:"keerthi",email:"keerthi123@gmail.com",age:20});
-user1.save();
+// user1.save();
 
-//insert many
-User.insertMany([{name:"anu",email:"anu123@gmail.com",age:17},
-    {name:"hema",email:"hema@gmail.com",age:18},
-    {name:"santha",email:"san@gmail.com",age:19}
-]);
+// insert many
+// User.insertMany([{name:"anu",email:"anu123@gmail.com",age:17},
+//     {name:"hema",email:"hema@gmail.com",age:18},
+//     {name:"santha",email:"san@gmail.com",age:19}
+// ]);
+
+//find
+// User.find({}).then((data)=>{
+//     console.log(data);
+// });
+
+//update one
+// User.updateOne({name:"keerthi"},{age:23}).then((data)=>{
+//     console.log(data);
+// })
+
+//update many
+// User.updateMany({age:{$gt:15}},{age:44}).then((data)=>{
+//     console.log(data);
+// })
+
+//find and update
+// User.findOneAndUpdate({name:"anu"},{age:66},{new:true}).then((data)=>{
+//     console.log(data);
+// })
+
+//delete many
+// User.deleteMany({name:"keerthi"}).then((data)=>{
+//     console.log(data);
+// })
+
+// User.deleteMany({age:{$gt:20}}).then((data)=>{
+//     console.log(data);
+// })
+
+//find one and delete
+User.findOneAndDelete({name:"anu"}).then((data)=>{
+    console.log(data);
+})
+
+
+

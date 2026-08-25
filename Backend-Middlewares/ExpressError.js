4d4f82e5adeeaa1941c7525baebb7e3fc2dfb7a0
Danda Keerthi <dandakeerthi2@gmail.com>
2026-08-25 20:56:19 +0530

    Middlewares

---
+++ Backend-Middlewares/ExpressError.js
@@ -0,0 +1,10 @@
+//custom error classes
+class ExpressError extends Error{
+    constructor(status,message){
+        super();
+        this.status=status;
+        this.message=message;
+    }
+}
+
+module.exports=ExpressError;
--- Backend-Middlewares/app.js
+++ Backend-Middlewares/app.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const app=express();
+const ExpressError=require("./ExpressError");
 
 //Middlewares
 // app.use(()=>{
@@ -23,7 +24,9 @@ app.use("/api",(req,res,next)=>{
     if(token === "giveaccess"){
         next();
     }else{
-        res.send("Access Denied !!");
+        // res.send("Access Denied !!");
+        //throw new Error("Access Denied");
+        throw new ExpressError(401,"Access Denied");
     }
 })
 app.get("/api",(req,res)=>{
@@ -37,6 +40,32 @@ app.get("/",(req,res)=>{
 app.get("/random",(req,res)=>{
     res.send("This is a random root");
 });
+
+app.get("/err",(req,res)=>{
+    abc=abc;
+});
+
+// //Error handling Middlewares
+// app.use((err,req,res,next)=>{
+//     console.log("------Error------");
+//     next(err);
+// })
+
+//create a admin route and send an error with a 403 status code
+app.get("/admin",(req,res)=>{
+    throw new ExpressError(403,"Access Forbidden");
+})
+//Default status and message
+
+app.use((err,req,res,next)=>{
+    let {status,message}=err;
+    res.status(status).send(message);
+})
+
+
+// app.use((req,res)=>{
+//     res.status(404).send("PAGE NOT FOUND!!");
+// })
 
 app.listen(8080,()=>{
     console.log("Server is listening !");
